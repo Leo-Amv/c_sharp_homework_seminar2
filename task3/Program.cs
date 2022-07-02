01a8581aab14ac84b4703186ddd8d832a5e3f264
Leo_Amv <leonidamvrosov@yandex.ru>
2022-07-02 08:01:43 +0500

    Task 3 completed!

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Enter one digit from 1 to 7: "); // от 1 до 7 включительно
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number == 6 || number == 7)
+        {
+            Console.WriteLine("Today is a weekend day!");
+        }
+        else
+        {
+            Console.WriteLine("Today is a working day!");
+        }
+    }
+}
