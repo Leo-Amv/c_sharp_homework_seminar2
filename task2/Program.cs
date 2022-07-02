8ee23b493a795b9bc60d28d3824ab5ee2ffd3fb2
Leo_Amv <leonidamvrosov@yandex.ru>
2022-07-02 07:42:07 +0500

    Task 2 completed!

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿internal class Program //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Enter the number: ");
+        string number = Console.ReadLine();            
+        if (number.Length < 3)
+        {
+            Console.WriteLine("Third digit does not exist!");
+        }
+        else
+        {
+            Console.WriteLine(number[2]);
+        }
+        
+    }
+}
